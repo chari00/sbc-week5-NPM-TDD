c23b181644671af90df7641fa4b284611e140b2b
chari00 <macharitocruzuk@gmail.com>
2024-10-20 19:58:40 +0100

    add validation for numbers that divide by zero.

--- index.js
+++ index.js
@@ -14,13 +14,16 @@ const calculate = () => {
       .replace(/x/g, "*")
       .replace(/÷/g, "/")
       .replace(/(\d+(\.\d+)?)%/g, (match, p1) => `(${p1}/100)`);
-    // console.log("Parsed expression:", expression);
+    console.log("Parsed expression:", expression);
+    if (expression.includes('/0')) {
+      throw new Error("Can't divide by zero");
+  }
 
     let result = eval(expression);
     // console.log("Result before formatting:", result);
     screenDisplay.value = Number(result.toFixed(10));
   } catch (error) {
-    screenDisplay.value = "Error";
+    screenDisplay.value = error.message ||"Error";
     // console.error("Calculation error:", error);
   }
 };
