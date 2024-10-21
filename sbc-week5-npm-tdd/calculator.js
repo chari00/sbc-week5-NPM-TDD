9134d56bbdfba842d2a807e02a173eea47065c7e
chari00 <macharitocruzuk@gmail.com>
2024-10-21 13:48:45 +0100

    convert project to vitest

--- sbc-week5-npm-tdd/calculator.js
+++ sbc-week5-npm-tdd/calculator.js
@@ -1,29 +1,24 @@
-const screenDisplay = document.getElementById("screen");
 
-const displayToScreen = (input) => {
+export const displayToScreen = (input, screenDisplay) => {
   screenDisplay.value += input;
 };
 
-const clearScreen = () => {
+export const clearScreen = (screenDisplay) => {
   screenDisplay.value = "";
 };
 
-const calculate = () => {
+export const calculate = (screenDisplay) => {
   try {
     let expression = screenDisplay.value
       .replace(/x/g, "*")
       .replace(/÷/g, "/")
       .replace(/(\d+(\.\d+)?)%/g, (match, p1) => `(${p1}/100)`);
-    console.log("Parsed expression:", expression);
     if (expression.includes('/0')) {
       throw new Error("Can't divide by zero");
-  }
-
+    }
     let result = eval(expression);
-    // console.log("Result before formatting:", result);
     screenDisplay.value = Number(result.toFixed(10));
   } catch (error) {
-    screenDisplay.value = error.message ||"Error";
-    // console.error("Calculation error:", error);
+    screenDisplay.value = error.message || "Error";
   }
 };
